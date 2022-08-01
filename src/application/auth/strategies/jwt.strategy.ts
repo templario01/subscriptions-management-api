7c6939956c53731a1e5333b348b00db2d0b4fdf1
victor benavente <templario0141@gmail.com>
2022-08-01 09:30:37 -0500

    refact: update JwtStrategy 🔑

--- src/application/auth/strategies/jwt.strategy.ts
+++ src/application/auth/strategies/jwt.strategy.ts
@@ -2,24 +2,19 @@ import { ExtractJwt, Strategy } from 'passport-jwt'
 import { PassportStrategy } from '@nestjs/passport'
 import { Injectable } from '@nestjs/common'
 import { SessionData } from '../dtos/response/auth.response'
+import { EnvConfigService } from '../../../config/env-config.service'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly configService: EnvConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.jwtConfig().jwtSecretKey,
       ignoreExpiration: false,
-      secretOrKey: 'secret',
-      logging: true,
     })
   }
 
   async validate(payload: SessionData) {
-    return {
-      userId: payload.id,
-      username: payload.username,
-      phone: payload.phone,
-      roles: payload.roles,
-    }
+    return payload
   }
 }
